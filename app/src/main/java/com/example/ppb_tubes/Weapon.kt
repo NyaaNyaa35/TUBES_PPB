e2bd97453ad66b66ce93f1d1402a9ee1057c6941
HansNotFound <if-19036@students.ithb.ac.id>
2022-03-23 17:59:10 +0700

    Add Weapon ShowCase

---
+++ app/src/main/java/com/example/ppb_tubes/Weapon.kt
@@ -0,0 +1,11 @@
+package com.example.ppb_tubes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Weapon : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weapon)
+    }
+}
