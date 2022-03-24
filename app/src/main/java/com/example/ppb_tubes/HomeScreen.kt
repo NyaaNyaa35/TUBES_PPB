b69adf14ed9b71adf6dbdf74420165684903e379
HansNotFound <if-19036@students.ithb.ac.id>
2022-03-24 16:13:47 +0700

    Add Knife and Classic UI

--- app/src/main/java/com/example/ppb_tubes/HomeScreen.kt
+++ app/src/main/java/com/example/ppb_tubes/HomeScreen.kt
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 
 class HomeScreen : AppCompatActivity() , View.OnClickListener {
     companion object {
---
+++ app/src/main/java/com/example/ppb_tubes/WeaponView/TacticalKnife.kt
@@ -0,0 +1,12 @@
+package com.example.ppb_tubes.WeaponView
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ppb_tubes.R
+
+class TacticalKnife : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tactical_knife)
+    }
+}
