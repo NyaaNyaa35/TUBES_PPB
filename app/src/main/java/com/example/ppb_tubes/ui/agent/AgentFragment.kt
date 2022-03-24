f5136645c3f5af590e2bfaaa6fe903d56d28df97
Rafael371 <rafaelc.gracia@gmail.com>
2022-03-24 19:06:11 +0700

    menu main nav belum disatuin, push sementara ngeri crash lagi

---
+++ app/src/main/java/com/example/ppb_tubes/ui/agent/AgentFragment.kt
@@ -0,0 +1,37 @@
+package com.example.ppb_tubes.ui.agent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.ppb_tubes.databinding.FragmentAgentBinding
+
+class AgentFragment : Fragment() {
+
+    private var _binding: FragmentAgentBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val galleryViewModel =
+            ViewModelProvider(this).get(AgentViewModel::class.java)
+
+        _binding = FragmentAgentBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
