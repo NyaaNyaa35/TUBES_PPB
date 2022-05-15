dc002258c5d1c63f346d14443857ad11c9317bf6
Rafael371 <rafaelc.gracia@gmail.com>
2022-05-15 20:25:09 +0700

    navbar on agent and weapon

--- app/src/main/java/com/example/ppb_tubes/AgentView/Brimstone.kt
+++ app/src/main/java/com/example/ppb_tubes/AgentView/Brimstone.kt
@@ -2,11 +2,39 @@ package com.example.ppb_tubes.AgentView
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.transition.TransitionManager
 import com.example.ppb_tubes.R
+import com.example.ppb_tubes.SideNavFragment
 
-class Brimstone : AppCompatActivity() {
+class Brimstone : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_brimstone)
+
+        val sideNav: ImageView = findViewById(R.id.side_nav)
+        sideNav.setOnClickListener(this)
+    }
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.side_nav -> {
+                val mFragmentManager = supportFragmentManager
+                val mSideNavFragment = SideNavFragment()
+
+                val rootView: ViewGroup = findViewById(R.id.brimstone_layout)
+                val mFade: androidx.transition.Fade =
+                    androidx.transition.Fade(androidx.transition.Fade.IN)
+                TransitionManager.beginDelayedTransition(rootView, mFade)
+
+                mFragmentManager
+                    .beginTransaction()
+                    .add(R.id.brimstone_layout, mSideNavFragment,
+                        SideNavFragment::class.java.simpleName)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/ppb_tubes/AgentView/Chamber.kt
+++ app/src/main/java/com/example/ppb_tubes/AgentView/Chamber.kt
@@ -2,11 +2,40 @@ package com.example.ppb_tubes.AgentView
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.transition.TransitionManager
 import com.example.ppb_tubes.R
+import com.example.ppb_tubes.SideNavFragment
 
-class Chamber : AppCompatActivity() {
+class Chamber : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chamber)
+
+        val sideNav: ImageView = findViewById(R.id.side_nav)
+        sideNav.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.side_nav -> {
+                val mFragmentManager = supportFragmentManager
+                val mSideNavFragment = SideNavFragment()
+
+                val rootView: ViewGroup = findViewById(R.id.chamber_layout)
+                val mFade: androidx.transition.Fade =
+                    androidx.transition.Fade(androidx.transition.Fade.IN)
+                TransitionManager.beginDelayedTransition(rootView, mFade)
+
+                mFragmentManager
+                    .beginTransaction()
+                    .add(R.id.chamber_layout, mSideNavFragment,
+                        SideNavFragment::class.java.simpleName)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
     }
 }
